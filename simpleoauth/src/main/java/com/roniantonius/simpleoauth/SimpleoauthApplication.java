043551823a1d9da0bba5efac0b8a1612ae34f0e8
roniantonius <roniantoniuss@upi.edu>
2025-04-03 21:57:33 +0700

    First init project implemented Google OAuth2 Security Configuration

---
+++ simpleoauth/src/main/java/com/roniantonius/simpleoauth/SimpleoauthApplication.java
@@ -0,0 +1,13 @@
+package com.roniantonius.simpleoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleoauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleoauthApplication.class, args);
+	}
+
+}
---
+++ simpleoauth/src/main/java/com/roniantonius/simpleoauth/controllers/BerandaController.java
@@ -0,0 +1,16 @@
+package com.roniantonius.simpleoauth.controllers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BerandaController {
+	@GetMapping(path = "/")
+	public String beranda(Model model, @AuthenticationPrincipal OAuth2User user) {
+		model.addAttribute("userName", user.getAttribute("name"));
+		return "index";
+	}
+}
